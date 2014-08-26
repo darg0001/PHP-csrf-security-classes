d3653c542c8cb210204e9e420d2b9757bed174e8
Olivier Beg <olivier@hotmail.lv>
2014-08-26 12:52:47 +0200

    $this->_tokens to $_SESSION['security_csrf']
    The $this->_tokens variables are not persistent, I had to put them in a
    session to make sure the tokens are.

--- security.csrf.php
+++ security.csrf.php
@@ -1,14 +1,27 @@
 <?php
-namespace security {
 
- class CSRF {
+namespace security 
+{
+
+ class CSRF 
+ {
   
   private $_token;
-  private $_tokens = [];
-  private $_time = 3600;
+  private $_tokens;
+  private $_time =  3600;
 
+  public function __construct() 
+  {
+	
+	if(!isset($_SESSION['security_csrf'])) 
+	{
+	 
+	 $_SESSION['security_csrf'] = [];
+	}
+  }
   
-  public function set_time($time) {
+  public function set_time($time) 
+  {
     
 	/*
     * @variables: Time is in seconds;
@@ -16,7 +29,8 @@ namespace security {
     * @Desc: Set the time until the CSRF token expires.
     */
 	
-    if(is_int($time) || is_numeric($time)) {
+    if(is_int($time) || is_numeric($time)) 
+	{
 		
 		$this->_time = $time;
 		
@@ -26,22 +40,26 @@ namespace security {
    return false;
   }
   
-  public function delete($token) {
+  public function delete($token) 
+  {
 	
-   if($this->verify($token)) {
+   if($this->verify($token)) 
+   {
 	
-    array_diff($this->_tokens, [ $token ]);
+    array_diff($_SESSION['security_csrf'], [ $token ]);
 	
 	$clean = [];
 	
-	foreach($this->_tokens AS $token => $time) {
+	foreach($_SESSION['security_csrf'] AS $token => $time) 
+	{
 		
-		if($time >= time()) {
+		if($time >= time()) 
+		{
 			$clean[] = $token;
 		}
 	}
 	
-	array_diff($this->_tokens, [ $clean ]);
+	array_diff($_SESSION['security_csrf'], [ $clean ]);
 	
 	return true;
    } else {
@@ -49,21 +67,23 @@ namespace security {
    }
   }
   
-  public function create($time = true) {
+  public function create($time = true) 
+  {
   
    /*
    * @By: Olivier Beg
    * @Desc: Generates a token to verify that the POST/GET request is legit and returns the last created token.
    */
    $this->_token = sha1(mt_rand() . rand() . md5($_SERVER['REMOTE_ADDR']));
-   $this->_tokens[$this->_token] = (time() + ($time ? $this->_time : (int) $time));
+   $_SESSION['security_csrf'][$this->_token] = (time() + ($time ? $this->_time : (int) $time));
    
-   return $this->_tokens[$this->_token];
+   return $_SESSION['security_csrf'][$this->_token];
   }
   
-  public function verify($token) {
+  public function verify($token) 
+  {
 	
-   return isset($this->_tokens[$token]);
+   return isset($_SESSION['security_csrf'][$token]);
   }
  }
 }
