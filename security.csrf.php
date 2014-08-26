deb1779e4377087408f1df0b05e0faba0066ada8
Olivier Beg <olivier@hotmail.lv>
2014-08-26 11:54:46 +0200

    Updated the CSRF class
    Tabs to spaces.
    Added a "set_time" function, it gives the time till the token expires!

--- security.csrf.php
+++ security.csrf.php
@@ -1,22 +1,44 @@
 <?php
 namespace security {
-	
+
  class CSRF {
    
-   private $_tokens = [];
-   
-   public function create() {
-    /*
-	* @By: Olivier Beg
-	* @Desc: Generate a token to verificate that the POST/GET request is legit.
+  private $_tokens = [];
+  private $_time = 3600;
+  
+  public function set_time($time) {
+	
+	/*
+	* @variables: Time is in seconds;
+	* @by: Olivier Beg
+	* @Desc: Set the time until the CSRF token expires.
 	*/
 	
-	$this->_tokens[] = sha1(mt_rand() . rand() . md5($_SERVER['REMOTE_ADDR']));
-   }
-   
-   public function verificate($token) {
+	if(is_int($time) || is_numeric($time)) {
+		
+		$this->_time = $time;
+		
+		return true;
+	}
 	
-	return (isset($this->_tokens[$token]) ? true : false);
-   }
+	return false;
+  }
+  
+  public function create() {
+  
+   /*
+   * @By: Olivier Beg
+   * @Desc: Generate a token to verify that the POST/GET request is legit.
+   */
+	
+   $this->_tokens[sha1(mt_rand() . rand() . md5($_SERVER['REMOTE_ADDR']))] = (time() + $this->_time);
+   
+   return end($this->_tokens);
+  }
+  
+  public function verify($token) {
+	
+   return isset($this->_tokens[$token]);
+  }
  }
 }
