95cc306c36739e8f01c816f9b1e7dcc843079a76
Olivier Beg <olivier@hotmail.lv>
2014-08-26 11:56:46 +0200

    Updatet the CSRF class
    tabs to spaces

--- security.csrf.php
+++ security.csrf.php
@@ -7,21 +7,21 @@ namespace security {
   private $_time = 3600;
   
   public function set_time($time) {
-	
+    
 	/*
-	* @variables: Time is in seconds;
-	* @by: Olivier Beg
-	* @Desc: Set the time until the CSRF token expires.
-	*/
+    * @variables: Time is in seconds;
+    * @by: Olivier Beg
+    * @Desc: Set the time until the CSRF token expires.
+    */
 	
-	if(is_int($time) || is_numeric($time)) {
+    if(is_int($time) || is_numeric($time)) {
 		
 		$this->_time = $time;
 		
 		return true;
-	}
-	
-	return false;
+    }
+
+   return false;
   }
   
   public function create() {
